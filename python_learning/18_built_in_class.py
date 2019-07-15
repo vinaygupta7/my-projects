60edd686041e2fd289aa6fcbe91b1737fa4daa07
vinaygupta7 <vinaygupta.cse@gmail.com>
2019-07-15 23:57:27 +0530

    python builtin class

---
+++ python_learning/18_built_in_class.py
@@ -0,0 +1,10 @@
+class Builtin:
+      '''Explaining Builtin Class '''
+      count=0
+
+
+print("Builtin.__dict__: ", Builtin.__dict__)
+print("Builtin.__name__: ", Builtin.__name__)
+print("Builtin.__doc__: ", Builtin.__doc__)
+print("Builtin.__module__: ", Builtin.__module__)
+print("Builtin.__bases__: ", Builtin.__bases__)
