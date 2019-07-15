f83b7b1698ad628f89e81109c6493c6e03425326
vinaygupta7 <vinaygupta.cse@gmail.com>
2019-07-16 00:09:15 +0530

    python pub pri protected methods

--- python_learning/19_pub_pri_pro.py
+++ python_learning/19_pub_pri_pro.py
@@ -4,7 +4,14 @@ class Myclass():
         self._pro=("I'm protected")
         self.pub=("I'm public")
 
+    def __privateMethod(self):
+        print ("In Private method")
+
 ob=Myclass()
+
 print ob.pub
 print ob._pro
 #print ob.__pri
+
+print ob._Myclass__pri
+ob._Myclass__privateMethod()
